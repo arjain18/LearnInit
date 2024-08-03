93c12de622438e85abe3c7b8e6182e4fc3cf8259
Ashish Jain <arjain18@gmail.com>
2024-08-03 09:30:14 +0530

    learn init - setter property

--- LearnInit/Program.cs
+++ LearnInit/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LearnInit
+﻿using System.Runtime.CompilerServices;
+
+namespace LearnInit
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student { Id = 23 };
+            student.Name = "aj";
+            //student.Id = 23;
+            student.Standard = 34;
+            Console.WriteLine(student.Id + " " + student.Name + " " + student.Standard);
         }
+     }
+
+    public class Student
+    {
+        //init introduce in C# 9
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public int Standard {  get; set; }
+        //public Student(int Id)
+       // {
+       //     this.Id = Id;
+      //  }
     }
 }
